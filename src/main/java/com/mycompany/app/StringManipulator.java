415139de60fb245ca779f46e23ef9e97c82c2dc4
Manjunath Shankara Reddy <mshankar@akamai.com>
2018-06-21 22:37:59 -0400

    getWeight, hasUniqueCharacters, sortStringsByWeight.

--- src/main/java/com/mycompany/app/StringManipulator.java
+++ src/main/java/com/mycompany/app/StringManipulator.java
@@ -1,13 +1,73 @@
 package com.mycompany.app;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  *
  */
 public class StringManipulator
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    static final String filterNonAlphaRegex = "[^A-Za-z]";
+
+    public static String cleanString(final String str) {
+        assert(str !=null);
+        return str.replaceAll(filterNonAlphaRegex, "").toUpperCase();
+    }
+
+    public static Boolean hasUniqueChars(final String word){
+        assert(word !=null);
+        char[] alphaString = cleanString(word).toCharArray();
+        Arrays.sort(alphaString);
+        for(int i=0; i<alphaString.length-1; i++)
+        {
+            if (alphaString[i] == alphaString[i+1])
+                return false;
+        }
+        return true;
+    }
+
+    public static float getWeight(final String word) {
+        assert(word !=null);
+        int weight =0;
+        String alphaString= word.replaceAll(filterNonAlphaRegex, "").toUpperCase();
+        for (char c : alphaString.toCharArray()) {
+         weight = weight + (int)c;
+        }
+        return ((float) weight/(float) alphaString.length());
+    }
+
+    static class StringWeightCompartor implements Comparator<String> {
+
+        public int compare(String o1, String o2) {
+            if (getWeight(o1) > getWeight(o2))
+                    return 1;
+            else if (getWeight(o1) < getWeight(o2))
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    public static String[] sortStrings(final String[] words){
+        String[] sortedWords = words;
+        Arrays.sort(sortedWords, new StringWeightCompartor());
+        return sortedWords;
+    }
+
+    public static void main( String[] args ) {
+
+        String[] input = {"RED", "APPLE", "BLUE", "COMPUTER"};
+
+        Stream.of(input).forEach(x -> {
+            System.out.println(x +", " + hasUniqueChars(x) + "," + getWeight(x));
+        });
+
+
     }
 }
--- src/test/java/com/mycompany/app/StringManipulatorTest.java
+++ src/test/java/com/mycompany/app/StringManipulatorTest.java
@@ -4,35 +4,57 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.stream.Stream;
+
 /**
  * Unit test for simple StringManipulator.
  */
 public class StringManipulatorTest
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+
     public StringManipulatorTest(String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
+
     public static Test suite()
     {
         return new TestSuite( StringManipulatorTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+
+    public void testHasUniqueChars()
     {
-        assertTrue( true );
+
+        assertTrue(StringManipulator.hasUniqueChars("RED"));
+        assertFalse(StringManipulator.hasUniqueChars("APPLE"));
+        assertTrue(StringManipulator.hasUniqueChars("BLUE"));
+        assertTrue(StringManipulator.hasUniqueChars("COMPUTER"));
+
     }
+
+    public void testGetWeight()
+    {
+        assertEquals(StringManipulator.getWeight("RED"), 73.0f);
+        assertEquals(StringManipulator.getWeight("APPLE"), 74.0f);
+        assertEquals(StringManipulator.getWeight("BLUE"), 74.0f);
+        assertEquals(StringManipulator.getWeight("COMPUTER"), 77.875f);
+
+    }
+
+    public void testSortStrings()
+    {
+        String[] inputWords = {"APPLE", "BLUE", "COMPUTER", "RED"};
+        String[] expected = {"RED", "APPLE", "BLUE", "COMPUTER"};
+        String[] sortedWords = StringManipulator.sortStrings(inputWords);
+        for (int i = 0; i < sortedWords.length; i++) {
+
+            assertEquals(sortedWords[i], expected[i]);
+        }
+
+    }
+
+
 }
